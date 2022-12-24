d9c1e551c3614e6a1a4235b01f2b76dcde9a00dd
Mereba11 <abracadabragio12@gmail.com>
2022-12-24 21:05:17 +0400

    Update additional.py

--- cogs/additional.py
+++ cogs/additional.py
@@ -1,8 +1,10 @@
 import random
+import string
 
 import discord
 from discord.ext import commands
 from discord import Embed
+from discord import app_commands
 
 from utils.http import session
 from utils.bot import Brains
@@ -34,6 +36,25 @@ class Additional(commands.Cog):
         embed.color = random.choice(QUOTE_COLORS)
         await ctx.send(embed=embed)
 
+    @app_commands.command(name='password', description='Generates random strong password')
+    @app_commands.describe(
+        length='The length of the password(less than 100)'
+        )
+    async def _password(self, interaction: discord.Interaction, length: int):
+        password = ''.join(random.choices(string.ascii_lowercase + string.ascii_uppercase + string.digits,
+                k=length
+            ))
+
+        if length > 100:
+            await interaction.response.send_message('Passoword length can\'t be longer than 100')
+        else:
+            try:
+                await interaction.user.send(f'Your password is `{password}`')
+                await interaction.response.send_message('Your password was sent in DMs')
+            except:
+                await interaction.response.send_message(f'Your password is `{password}`', ephemeral=True)
+        
+    
 
 
 async def setup(bot: Brains):
